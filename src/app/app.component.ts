1aa6bd2d9011612533eaaacad215d6b59d895e56
Doooodie <unfair.slenderman@gmail.com>
2023-12-22 00:55:25 +0100

    feat: add search results layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SearchResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SearchItemComponent } from '../search-item/search-item.component';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchItemComponent],
   templateUrl: './search-results.component.html',
 })
 export class SearchResultsComponent {}
