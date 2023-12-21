fdec6241d662cc4092f62ca8e694f01aad036fb8
Doooodie <unfair.slenderman@gmail.com>
2023-12-21 21:58:30 +0100

    feat: add header layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LogoComponent } from './logo/logo.component';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LogoComponent, SearchInputComponent, ProfileComponent, SettingsComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
