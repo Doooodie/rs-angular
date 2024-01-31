7eeb3b6585272a11861bf6f8b33da9e6f249c5e6
Doooodie <unfair.slenderman@gmail.com>
2024-01-31 21:37:37 +0100

    feat: add layout to 404 page

--- src/app/core/pages/page-not-found/page-not-found.component.ts
+++ src/app/core/pages/page-not-found/page-not-found.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFaceSadTear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './page-not-found.component.html',
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+  faFaceSadTear = faFaceSadTear;
+}
